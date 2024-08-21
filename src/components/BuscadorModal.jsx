72ef69fb283be923db583e19c1e4880edd42b1e9
JorgeNavarroN <jnnavarroxd@gmail.com>
2024-08-21 17:00:20 -0500

    bug de imagen de buscar fixed

--- src/components/BuscadorModal.jsx
+++ src/components/BuscadorModal.jsx
@@ -1,5 +1,6 @@
 import { useAlert } from "../services/utils/hooks/UseAlert"
 import { SearchAlert } from "./SearchAlert"
+import search from "../assets/search-32.png"
 import '../index.css'
 import PropTypes from 'prop-types'
 
@@ -45,7 +46,7 @@ export const BuscadorModal = ({ nameClass, opacity, handleClose, handleSearch })
         <form onSubmit={handleSubmitSearch} className="bg-slate-700 flex flex-row text-white mx-auto box-border drop-shadow-lg backdrop-blur-xl rounded-full transition-colors duration-200">
           <input style={{}} autoFocus={true} name="busqueda" onChange={handleOnChangeInput} onClick={handleClickInput} className="bg-transparent min-h-full px-5 min-w-96 text-lg max-sm:text-sm max-sm:min-w-60 outline-none" type="text" placeholder="Londres, México, Perú ..." />
           <button className="border-l border-white/10 hover:bg-slate-900 rounded-r-full px-5 py-3 transition-colors cursor-pointer">
-            <img width={28} height={28} src="/src/assets/search-32.png" alt="search-32.png" />
+            <img width={28} height={28} src={search} alt="search-32.png" />
           </button>
         </form>
         <SearchAlert text={textAlert} showAlert={showAlert} alertOpacity={alertOpacity} />
--- src/components/SearchAlert.jsx
+++ src/components/SearchAlert.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types'
+import alert24 from "../assets/alert-24.png"
 
 export const SearchAlert = ({ text, showAlert, alertOpacity }) => {
   return (
     <div hidden={showAlert} className={`opacity-${alertOpacity} transition-all duration-200`}>
       <span className="flex flex-row gap-2 justify-center">
-        <img src="/src/assets/alert-24.png" alt="" />
+        <img src={alert24} alt="" />
         <span className="text-red-600">{text}</span>
       </span>
     </div>
